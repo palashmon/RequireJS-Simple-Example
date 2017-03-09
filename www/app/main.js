b557f6a5344edcbe9b5bcfa82ff38a35d2d6c5c5
Palash Mondal <palashm@live.in>
2017-03-09 21:08:21 +0530

    Update main.js

--- www/app/main.js
+++ www/app/main.js
@@ -1,8 +1,8 @@
 require.config({
-  paths: {
-      // specify a path to jquery, the second declaration is the local fallback
-      jquery: ["https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min"]
-  }
+	paths: {
+		// specify a path to jquery, the second declaration is the local fallback
+		jquery: ["https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min"]
+	}
 });
 
 define(function (require) {
@@ -10,16 +10,16 @@ define(function (require) {
 	//debugger;
 	console.log("JS : main");
 
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var messages = require('./messages');
+	// Load any app-specific modules
+	// with a relative require call,
+	// like:
+	var messages = require('./messages');
 
-    // Load library/vendor modules using
-    // full IDs, like:
-    var p = require('print');
+	// Load library/vendor modules using
+	// full IDs, like:
+	var p = require('print');
 	var c = require('./console');
-    
+
 	var msg = messages.getHello();
-    p.print(msg);
+	p.print(msg);
 });
